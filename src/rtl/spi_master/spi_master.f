59c2d649c5f1e1bf8f6ed045b2ee63dbc4aed05f
Muneeb <muneebullashariff@gmail.com>
2022-01-22 12:46:45 +0530

    Adding the spi_master RTL

---
+++ src/rtl/spi_master/spi_master.f
@@ -0,0 +1,9 @@
++incdir+${SPI_MASTER}
+${SPI_MASTER}/apb_spi_master.sv
+${SPI_MASTER}/spi_master_apb_if.sv
+${SPI_MASTER}/spi_master_clkgen.sv
+${SPI_MASTER}/spi_master_controller.sv
+${SPI_MASTER}/spi_master_fifo.sv
+${SPI_MASTER}/spi_master_rx.sv
+${SPI_MASTER}/spi_master_tx.sv
+

0eae6fe8788444c930aec9c9f358559f0ff971df
anithasamanthula <anithasamanthula@mirafra.com>
2022-02-23 11:12:08 +0530

    added few more testcases

--- src/dv/hvl_top/test/sequences/master_sequences/reg_sequences/apb_reg_seq_pkg.sv
+++ src/dv/hvl_top/test/sequences/master_sequences/reg_sequences/apb_reg_seq_pkg.sv
@@ -42,8 +42,13 @@ package apb_reg_seq_pkg;
   `include "apb_master_std_mode_read_thtx_rhtx_cnttx_cntrx_value_2_reg_seq.sv"
 
   `include "apb_master_std_mode_write_0_cmd_0_addr_32_data_length_reg_seq.sv"
+  `include "apb_master_std_mode_write_0_cmd_0_addr_16_data_length_reg_seq.sv"
+  `include "apb_master_std_mode_write_0_cmd_16_addr_16_data_length_reg_seq.sv"
+  `include "apb_master_std_mode_write_16_cmd_16_addr_16_data_length_reg_seq.sv"
   `include "apb_master_std_mode_write_8_cmd_8_addr_32_data_length_reg_seq.sv"
   `include "apb_master_std_mode_write_8_cmd_16_addr_32_data_length_reg_seq.sv"
+  `include "apb_master_std_mode_write_8_cmd_32_addr_32_data_length_reg_seq.sv"
+  `include "apb_master_std_mode_write_16_cmd_16_addr_32_data_length_reg_seq.sv"
   `include "apb_master_std_mode_write_0_cmd_32_addr_32_data_length_reg_seq.sv"
   `include "apb_master_std_mode_write_32_cmd_32_addr_32_data_length_reg_seq.sv"
 
